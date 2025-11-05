b1898e01a859bff900db5cc5ad398e30d0e42041
Eduardo Moyano <cba2486@gmail.com>
2025-11-05 16:04:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7981936388-b0707",
+  "appId": "1:997544751443:web:8e7b67c1b8cc6aff01f1a6",
+  "apiKey": "AIzaSyCuE1PJ2lW8koAJ7IZ1UFX-TTs_hii-wGI",
+  "authDomain": "studio-7981936388-b0707.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "997544751443"
+};
